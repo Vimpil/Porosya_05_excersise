7d93fe65d33bd3c4c0f2e3b2fca81425aa9cc02b
Vimpil <vimpil@ya.ru>
2016-12-30 16:08:32 +0300

    header first draft made

---
+++ js/script.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+
+	// timer for offer
+
+	function getTimeRemaining(endtime) {
+		var t = Date.parse(endtime) - Date.parse(new Date());
+		var seconds = Math.floor((t / 1000) % 60);
+		var minutes = Math.floor((t / 1000 / 60) % 60);
+		var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+		var days = Math.floor(t / (1000 * 60 * 60 * 24));
+		return {
+			'total': t,
+			'days': days,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds
+		};
+	}
+
+	function initializeClock(id, endtime) {
+		var clock = document.getElementById(id);
+		var daysSpan = clock.querySelector('.days');
+		var hoursSpan = clock.querySelector('.hours');
+		var minutesSpan = clock.querySelector('.minutes');
+		var secondsSpan = clock.querySelector('.seconds');
+
+		function updateClock() {
+			var t = getTimeRemaining(endtime);
+
+			daysSpan.innerHTML = t.days;
+			hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+			minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+			secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+			if (t.total <= 0) {
+				clearInterval(timeinterval);
+			}
+		}
+
+		updateClock();
+		var timeinterval = setInterval(updateClock, 1000);
+	}
+
+	var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+	initializeClock('clockdiv', deadline);
+
+	// END timer for offer
+
+	// Load more
+	var images = $(".handler > div").hide(), x = 1;
+    var showMore = $('#showMore');
+    var showLess = $('#showLess');
+    var funcs = {
+       'showMore': function() { ++x; show(); },
+       'showLess': function() { --x; show(); }
+    }
+    $('.controls').on('click', 'a', function(e){
+        return (funcs[e.target.id] || function(){})(), false;
+    });
+    function show() {
+        images.hide().filter(function(i){ return i < (x * 3); }).show();
+        showMore.show().filter(function(){ return !images.is(':hidden'); }).hide();
+        showLess.show().filter(function(){ return x === 1; }).hide();
+    }
+    show();
+    // END Load more
+
+});
